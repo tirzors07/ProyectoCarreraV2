463d879dbfdede70948787d3c2358e6827dc4c75
Ricardo Tirzo Sotero <160087448+tirzors07@users.noreply.github.com>
2025-12-29 20:56:20 -0800

    Update database credentials in config.js

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -3,14 +3,14 @@ dotenv.config({ path: '../../../.env' });
 
 
 export const PORT = process.env.PORT ?? 3000;
-export const DB_HOST = "localhost";//192.168.100.34";
-export const DB_USER = "a1282016";//"root";
-export const DB_PASSWORD = "admin123"//"natali123";
-export const DB_NAME = "a1282016_bd3";//"proyecto";
+export const DB_HOST = "localhost";//";
+export const DB_USER = "axxxxxxxx";//"root";
+export const DB_PASSWORD = "xxxxxx"//"natali123";
+export const DB_NAME = "xxxxxxxxxxxxxxxx";//"proyecto";
 export const SALT_ROUNDS = 10;
 export const SECRET_JWT_KEY = "shhhhh,NO,MENTIRA,ES,SECRETO";
 export const TCP_PORT = 6000;
-export const VALID_CLIENT_ID = "a1282016";
+export const VALID_CLIENT_ID = "axxxxxxxxx";
 export const VALID_ROOT_TOKEN = "root@777";
 
 console.log("ENV cargado:", {
@@ -20,3 +20,4 @@ console.log("ENV cargado:", {
   DB_NAME,
 
 });
+
